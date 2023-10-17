9d7208258e261e28cb325af3ae099bdd325abfb5
Michel Cuevas <michelcuevass11@gmail.com>
2023-10-17 09:50:26 -0500

    first commit

---
+++ APISistemaVenta/SistemaVenta.DTO/VentaInicialDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DTO
+{
+    public class VentaInicialDTO
+    {
+        public int IdVenta { get; set; }
+        public string? NumeroDocumento { get; set; }
+        public string? TipoPago { get; set; }
+        public string? TotalTexto { get; set; }
+        public string? FechaRegistro { get; set; }
+    }
+}
